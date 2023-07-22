c5e884e258dd77f407ea394635567849e15f74a1
Srimanta Kumar Acharjya <srimantakumar.sk24@gmail.com>
2023-07-22 16:59:09 +0530

    COMMIT

--- src/DSA/BINARYSEARCH/ifinitearray.java
+++ src/DSA/BINARYSEARCH/ifinitearray.java
@@ -1,33 +1,44 @@
 package DSA.BINARYSEARCH;
 
 public class ifinitearray {
-    static int infinite(int[] nums,int target){
-        int start=0;
-        int last=1;
-        while(target>nums[last]){
-            int temp=start;
-            last=last+((last-start)+1)*2;
-            temp=last+1;
-        }
-        return search(nums,start,last,target);
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(ans(arr, target));
     }
-    static int search(int[] nums,int start,int last,int target)
-    {
-        while (start<=last){
-            int mid=start+(last-start)/2;
-            if (target==nums[mid]){
+    static int ans(int[] arr, int target) {
+        // first find the range
+        // first start with a box of size 2
+        int start = 0;
+        int end = 1;
+
+        // condition for the target to lie in the range
+        while (target > arr[end]) {
+            int temp = end + 1; // this is my new start
+            // double the box value
+            // end = previous end + sizeofbox*2
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return binarySearch(arr, target, start, end);
+
+    }
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
                 return mid;
-            } else if (target<nums[mid]) {
-                last=mid-1;
-            }else{
-                start=mid+1;
             }
         }
         return -1;
-    }
-    public static void main(String[] args) {
-        int[] arr={1,2,3,4,5,6,7,8,9,10,11,12,13,14,23,34,56,67,89,90,101,102};
-        int pos=infinite(arr,14);
-        System.out.println(pos);
     }
 }
