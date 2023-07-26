5396cb1e5fc752300941963f309403ac94f5beed
Srimanta Kumar Acharjya <srimantakumar.sk24@gmail.com>
2023-07-26 09:05:55 +0530

    COMMIT

---
+++ src/DSA/ARRAY/TWOSUM.java
@@ -0,0 +1,55 @@
+package DSA.ARRAY;
+import java.util.*;
+public class TWOSUM {
+    public static String twoSum(int n, int []arr, int target) {
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                return "YES";
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return "NO";
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+    }
+}
+class TWOSUM1 {
+    public static int[] twoSum(int n, int []arr, int target) {
+        int[] ans = new int[2];
+        ans[0] = ans[1] = -1;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = arr[i];
+            int moreNeeded = target - num;
+            if (mpp.containsKey(moreNeeded)) {
+                ans[0] = mpp.get(moreNeeded);
+                ans[1] = i;
+                return ans;
+            }
+
+            mpp.put(arr[i], i);
+        }
+        return ans;
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        int[] ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 2: [" + ans[0] + ", "
+                + ans[1] + "]");
+
+    }
+
+}
